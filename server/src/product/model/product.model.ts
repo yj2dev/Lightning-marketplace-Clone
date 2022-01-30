b7cf82d301141a5b22e027334b0b0c5433783782
adu <nogo22@naver.com>
2022-01-31 05:11:27 +0900

    create product model

---
+++ server/src/product/model/product.model.ts
@@ -0,0 +1,74 @@
+import { Document, SchemaOptions } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+const options: SchemaOptions = {
+  timestamps: true,
+  versionKey: false,
+};
+
+@Schema(options)
+export class Product extends Document {
+  @Prop({ required: true })
+  @IsString()
+  @IsNotEmpty()
+  seller: string;
+
+  @Prop({ required: true })
+  @IsString()
+  @IsNotEmpty()
+  productImgURL: string;
+
+  @Prop({ required: true })
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @Prop({ required: true })
+  @IsString()
+  @IsNotEmpty()
+  category: string;
+
+  @Prop({ required: true })
+  @IsString()
+  @IsNotEmpty()
+  transactionRegion: string;
+
+  @Prop({ required: true })
+  @IsString()
+  @IsNotEmpty()
+  status: string;
+
+  @Prop({ required: true })
+  @IsString()
+  @IsNotEmpty()
+  exchange: string;
+
+  @Prop({ required: true })
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
+
+  @Prop({ required: true, default: false })
+  @IsBoolean()
+  @IsNotEmpty()
+  deliveryCharge: boolean;
+
+  @Prop({ required: true, minlength: 10, maxlength: 2000 })
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @Prop({})
+  @IsString()
+  @IsNotEmpty()
+  tag: string;
+
+  @Prop({})
+  @IsNumber()
+  @IsNotEmpty()
+  quantity: number;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
